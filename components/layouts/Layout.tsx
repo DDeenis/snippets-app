fec18be162867e9cde51de851bc21053ed59416a
DDeenis <ddeenis46@gmail.com>
2021-10-27 10:57:16 +0300

    update next

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -1,15 +1,10 @@
 import {Container} from '@chakra-ui/layout';
 import {NextPage} from 'next';
 import {Navigation} from './Navigation';
-import Head from 'next/head';
 
 export const Layout: NextPage = ({children}) => {
   return (
     <Container px={4} py={3} bg="brown" maxW="100%" minH="100vh">
-      <Head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </Head>
       <Navigation />
       {children}
     </Container>
