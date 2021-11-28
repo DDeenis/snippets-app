a0c236a1b385fad275ea82d03e8d06b5dc27139e
DDeenis <ddeenis46@gmail.com>
2021-11-28 13:25:56 +0200

    fix

---
+++ src/constants/routes.ts
@@ -0,0 +1,12 @@
+export const routes = {
+  nav: {
+    allSnippets: '/snippets',
+    mySnippets: '/snippets/personal',
+    createSnippet: '/snippets/create',
+    profile: '/profile',
+  },
+  auth: {
+    login: 'api/auth/login',
+    logout: 'api/auth/logout',
+  },
+};
